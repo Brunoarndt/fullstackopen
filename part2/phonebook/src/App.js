189845cba801c136e2130bef5c5bef19423daa81
Brunoarndt <brunoarndt73@gmail.com>
2023-04-28 15:39:17 -0300

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,24 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '00-00000-0000' 
-  }]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => { 
     setNewName(event.target.value)
   }
   const handleNumberChange = (e) => {
       setNewNumber(e.target.value)
-    }
+  }
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
   const handleSubmit = (event) => {
     event.preventDefault()
     if(persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())){
@@ -29,13 +35,15 @@ const App = () => {
     setNewNumber("")
   }
 
-  
-
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      
       <form onSubmit={handleSubmit}>
+        <h2>Add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
@@ -45,9 +53,18 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => {
-        return <p key={person.name}>{person.name} {person.number} </p>;
-      })}
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((person) => {
+          return (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          );
+        })}
+      
     </div>
   )
 }
