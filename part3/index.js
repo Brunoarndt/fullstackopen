5af6021619daaf55afc5b2cb2f7559bc5dc4c447
Brunoarndt <brunoarndt73@gmail.com>
2025-02-24 16:13:19 -0300

    exercise 3.4

--- part3/index.js
+++ part3/index.js
@@ -48,6 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(persons => persons.id != id)
+
+    response.status(204).end()
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
