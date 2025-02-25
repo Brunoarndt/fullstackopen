f217c660070ffd9d4d26b683559929ea963a293a
Brunoarndt <brunoarndt73@gmail.com>
2025-02-25 09:44:39 -0300

    exercise 3.7

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const app = express() 
+const morgan = require('morgan') 
+const app = express()
 const PORT = 3001
 
 let persons = [
@@ -29,6 +30,7 @@ const date = new Date()
 
 app.use(express.json())
 
+app.use(morgan('tiny'))
 
 app.post('/api/persons/:id', (request, response) => {
     const body = request.body
