25566072b31c6da66e26e9b2945748cdf9572fc6
Brunoarndt <brunoarndt73@gmail.com>
2025-02-26 19:05:09 -0300

    exercise 3.16

--- part3/index.js
+++ part3/index.js
@@ -12,6 +12,16 @@ let persons = []
 
 const date = new Date()
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -20,6 +30,10 @@ app.use(express.static('build'))
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -66,15 +80,19 @@ app.get('/api/persons/:id', (request, response) => {
         console.log("error finding the id");
         response.status(404).end()
     }
+    
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
-    .catch(error => console.log('error deleting'))
+    .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
