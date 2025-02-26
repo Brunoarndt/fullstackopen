2b7aea1a57a6b0aa10ed4e64d8c33aded27221ee
Brunoarndt <brunoarndt73@gmail.com>
2025-02-26 18:51:33 -0300

    exercise 3.15

--- part3/index.js
+++ part3/index.js
@@ -69,10 +69,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(persons => persons.id != id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => console.log('error deleting'))
 })
 
 app.listen(PORT, () => {
