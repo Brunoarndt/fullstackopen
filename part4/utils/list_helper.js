98bbd5845319a2397815b61f325c9e95dec103eb
Brunoarndt <brunoarndt73@gmail.com>
2025-03-07 14:29:45 -0300

    exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,8 +5,24 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    if (!blogs.length) {
+        return null
+    }
+
+    const reducer = (max, current) => {
+        if(current.likes > max.likes){
+            max = current
+        }
+        return max
+      }
+
+    return blogs.reduce(reducer, blogs[0])
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
