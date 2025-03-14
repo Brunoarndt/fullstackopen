0ce0ff7669f4294a418fa0cb7755433ddfdae6a6
Brunoarndt <brunoarndt73@gmail.com>
2025-03-14 14:49:18 -0300

    exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
@@ -21,15 +23,38 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  // const { title, author, url, likes, userId } = request.body
   const { title, author, url, likes } = request.body
-
   if (!title || !url) {
     return response.status(400).json({ error: 'Title and URL are required' })
   }
+  const user = await User.findOne()
+  // const user = await User.findById(userId)
 
-  const blog = new Blog({title, author, url, likes})
+  // if(!user){
+  //   return response.status(400).json({error:'Invalid user ID'})
+  // }
+
+  // const blog = new Blog({
+  //   title, 
+  //   author, 
+  //   url, 
+  //   likes,
+  //   user: user._id
+  //  })
+
+  const blog = new Blog({
+    title, 
+    author, 
+    url, 
+    likes,
+    user: user._id
+   })
 
   const savedBlog = await blog.save()
+
+  // user.blogs = user.blogs.concat(savedBlog._id)
+  // await user.save()
   response.status(201).json(savedBlog)
 })
 
